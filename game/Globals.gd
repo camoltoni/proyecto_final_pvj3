e00137a633640f08ed6848750873a3386df1a8e2
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2024-04-16 18:17:45 -0300

    Move game folder to aux

--- game/Globals.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var scene_index:int = 0
-var scenes_array:PoolStringArray = ["res://screens/StartScreen.tscn","res://screens/GameScreen.tscn"]
--- game/screens/StartScreen.gd
+++
@@ -1,5 +0,0 @@
-extends "res://screens/BaseScreen.gd"
-
-func _process(_delta):
-	if Input.is_action_pressed("ui_accept"):
-		end_scene()
