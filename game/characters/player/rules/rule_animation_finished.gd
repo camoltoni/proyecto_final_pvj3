5b43944f4847fff3a2b02b9b46e43bdcbd045910
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2025-01-22 19:58:16 -0300

    Revert "Agregadas animaciones idle-walk-put"
    This reverts commit ee15204f5901209506ea13382166872b396165b9.

--- game/characters/player/rules/rule_animation_finished.gd
+++
@@ -1,8 +0,0 @@
-extends Rule
-
-func check(params:Dictionary, character:Character):
-	if params.has("animation_name"):
-		if (character.animation_player as AnimationPlayer).current_animation == params["animation_name"]:
-			if !character.animation_player.is_playing():
-				return true
-	return false
